558b671fe59852cf39ee0d4fb5871beaafa18f5c
Nguyễn Quang Thắng <quangthangmzl@gmail.com>
2025-05-12 16:32:19 +0700

    .

---
+++ Client/TienLenDoAn/TienLenDoAn/SinglePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TienLenDoAn
+{
+    public partial class SinglePlayer : Form
+    {
+        public SinglePlayer(Form frm)
+        {
+            InitializeComponent();
+        }
+    }
+}
