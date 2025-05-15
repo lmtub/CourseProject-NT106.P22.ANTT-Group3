6cc6afdac92c19e281968b5874496fa75e5a92c6
Nguyễn Quang Thắng <quangthangmzl@gmail.com>
2025-05-15 09:02:56 +0700

    Update UI Create Room

--- Client/TienLenDoAn/TienLenDoAn/CreateOrJoin.cs
+++ Client/TienLenDoAn/TienLenDoAn/CreateOrJoin.cs
@@ -27,9 +27,15 @@ namespace TienLenDoAn
     
     public partial class CreateOrJoin : Form
     {
+        TcpClient client;
+        IPEndPoint serverIP; // địa chỉ IP của server
+        StreamReader sr;
+        StreamWriter sw;
+        Packet this_client_info = new Packet();
         
         public CreateOrJoin(string configPath)
         {
+            InitializeComponent();
             
         }
         
@@ -38,5 +44,60 @@ namespace TienLenDoAn
 
         }
         
+        
+
+         private void button1_Click(object sender, EventArgs e) // button Create New
+        { 
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) // button Join Room
+        {
+            
+
+        }
+        public bool IPv4IsValid(string ipv4) // hàm check IP
+        {
+            if (String.IsNullOrWhiteSpace(ipv4)) return false;
+
+            string[] splitValues = ipv4.Split('.');
+            if (splitValues.Length != 4) return false;
+
+            byte posNum;
+            return splitValues.All(i => byte.TryParse(i, out posNum));
+        }
+        private void button3_Click(object sender, EventArgs e) // button Start
+        {
+               
+        }
+        
+        public static byte[] converterDemo(Image x) 
+        {
+            ImageConverter _imageConverter = new ImageConverter();
+            byte[] xByte = (byte[])_imageConverter.ConvertTo(x, typeof(byte[]));
+            return xByte;
+        }
+        
+        
+
+        private void tbRoomID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbServerIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           // Application.OpenForms["Form1"].Close();
+        }
     }
 }
