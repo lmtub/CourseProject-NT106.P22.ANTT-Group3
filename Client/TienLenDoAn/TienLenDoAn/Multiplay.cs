5814a6004aaa6d88a8af762e16f816aea21b7cb5
Nguyễn Quang Thắng <quangthangmzl@gmail.com>
2025-05-15 09:14:23 +0700

    Update UI MultiPlayer

--- Client/TienLenDoAn/TienLenDoAn/Multiplay.cs
+++ Client/TienLenDoAn/TienLenDoAn/Multiplay.cs
@@ -1,20 +1,97 @@
-﻿using System;
+﻿using Server;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.CodeDom.Compiler;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
 
 namespace TienLenDoAn
 {
     public partial class Multiplay : Form
     {
-        public Multiplay()
+         public Multiplay(string Code, string RoomID, string Username, byte[] Avatar, string IP, bool RoomOwner)
         {
-            InitializeComponent();
+
+        }
+        private void button1_Click(object sender, EventArgs e) // Nút deal
+        {
+
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void tmrPlayer_Tick(object sender, EventArgs e) // hàm xử lý thời gian đến ngược
+        {
+           
+        }
+        private void Choseobj_Click(object sender, EventArgs e) // hàm thêm sự kiện click các lá bài
+        {
+           
+        }
+        private void cmdSkip_Click(object sender, EventArgs e)
+        {
+          
+        }
+        private void cmdPlay_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void anh1_Click(object sender, EventArgs e) // hàm xử lý khi click vào avatar (hiển thị textbox để gửi message)
+        {
+           
+        }
+        private void btnSend_Click(object sender, EventArgs e) // button Send message
+        {
+            
+        }
+        private void button1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+        private void button2_Click(object sender, EventArgs e) // button Exit
+        {
+
+             
+        }
+        private void cmdUnChose_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void pbxClock_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pbrRemainTime_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void tmrDownRight_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void pbxOpponent_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
